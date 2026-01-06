80e034c4ca16474a6759cf7857f0088667face71
cbs4385 <chris.strube@gmail.com>
2026-01-06 10:47:44 -0500

    Revert "Randomize default procedural seeds"
    This reverts commit 0532c80af509876a1730c0565c10cd6f53282e5b.

--- Assets/Scripts/Systems/MazeGridBehaviour.cs
+++ Assets/Scripts/Systems/MazeGridBehaviour.cs
@@ -127,12 +127,6 @@ namespace FaeMaze.Systems
 
         #region Unity Lifecycle
 
-        private static int GenerateRandomSeed()
-        {
-            int seed = System.Environment.TickCount;
-            return seed != 0 ? seed : (int)System.DateTime.Now.Ticks;
-        }
-
         private void Awake()
         {
             // Validate references
@@ -545,10 +539,6 @@ namespace FaeMaze.Systems
 
             if (usePlanarGenerator)
             {
-                planarGeneratorConfig.randomSeed = planarGeneratorConfig.randomSeed == 0
-                    ? GenerateRandomSeed()
-                    : planarGeneratorConfig.randomSeed;
-
                 // Use planar organic generator
                 mazeString = ForestMaze.PlanarForestMazeGenerator.GenerateMaze(
                     planarGeneratorConfig.gridWidth,
@@ -565,10 +555,6 @@ namespace FaeMaze.Systems
             }
             else
             {
-                generatorConfig.randomSeed = generatorConfig.randomSeed == 0
-                    ? GenerateRandomSeed()
-                    : generatorConfig.randomSeed;
-
                 // Use traditional Kruskal-based generator
                 if (!HasCachedTilesForConfig(generatorConfig) || cachedGeneratedTiles == null || cachedGeneratedSymbols == null)
                 {
@@ -960,14 +946,8 @@ namespace FaeMaze.Systems
             cachedMazeString = null;
             cachedEntranceEdges.Clear();
 
-            if (usePlanarGenerator)
-            {
-                planarGeneratorConfig.randomSeed = GenerateRandomSeed();
-            }
-            else
-            {
-                generatorConfig.randomSeed = GenerateRandomSeed();
-            }
+            // Use current time as random seed for variety
+            generatorConfig.randomSeed = System.DateTime.Now.Millisecond + (System.DateTime.Now.Second * 1000);
 
             // Regenerate maze
             InitializeFromGenerator();
